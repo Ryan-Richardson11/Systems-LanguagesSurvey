1d98dc784ed550b54401496508d39012a95f44d6
Ryan Richardson <ryanrich811@gmail.com>
2023-09-01 11:31:06 -0400

    Fully stubbed out Python to Java code

--- Week1/Project1.java
+++ Week1/Project1.java
@@ -1,8 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-        Scanner scanner = new Scanner(System.in);
+
+    public static void fact() {
         System.out.println("Enter a Positive Integer:");
+        Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        System.out.println("It's developer tea time!");
+        while (n > 0) {
+            System.out.println("Sorry, only postivie numbers, enter again: ");
+            n = scanner.nextint();
+        }
+        if (n == 0) {
+            System.out.println("The factorial of 0 is 1");
+        } else {
+            int f = 1;
+            for (int i = 1; i <= n; i++)
+                f *= i;
+            System.out.println("The factorial of" + n + "is" + f);
+        }
+    }
+
+    public static void main(String[] args) {
+        fact();
+    }
 
 }
