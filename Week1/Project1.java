ce04e95a6891fcdf460520774647d20402123bdb
Ryan Richardson <ryanrich811@gmail.com>
2023-09-01 10:18:00 -0400

    Starting code conversion. Python to Java

---
+++ Week1/Project1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a Positive Integer:");
+        int n = scanner.nextInt();
+
+}
