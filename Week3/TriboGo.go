7dc8fb93d803336c90da2bb0e69ff114c3228280
unknown <ryanrich811@gmail.com>
2023-09-14 09:56:00 -0400

    Conversion code from Python to Go

---
+++ Week3/TriboGo.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func tribonacci(n int) []int {
+	if n <= 0 {
+		return []int{}
+	} else if n == 1 {
+		return []int{0}
+	} else if n == 2 {
+		return []int{0, 1}
+	} else {
+		fib_seq := []int{1, 1, 1}
+		for i := 3; i < n; i++ {
+			next_num := fib_seq[i-1] + fib_seq[i-2] + fib_seq[i-3]
+			fib_seq = append(fib_seq, next_num)
+		}
+		return fib_seq
+	}
+}
+
+func main() {
+	fmt.Println(tribonacci(20))
+}
