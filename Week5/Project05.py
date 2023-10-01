573d1a3764802c38c50c3d868144fa7f0310b83f
unknown <ryanrich811@gmail.com>
2023-10-01 15:12:28 -0400

    Working program with added documentation

--- Week5/Project05.py
+++ Week5/Project05.py
@@ -1,12 +1,15 @@
 import os
 import random
 
+# Generates 100 random numbers between 0 and 1000 then returns one per line.
 def generateNumbers():
     randNums = []
     for i in range(0, 100):
         randNums.append(str(random.randint(0, 1000)))
     return "\n".join(randNums)
 
+# Checks if the folder exists, if it does all files and folder are deleted then recreated.
+# If the file does not exist, it is then created.
 def createFolder(folder):
     if os.path.exists(folder):
         for file in os.listdir(folder):
@@ -16,13 +19,12 @@ def createFolder(folder):
     
     os.mkdir(folder)
 
-def createFile(numbers,folder, file):
+# Creates new file with the inputed path and writes the numbers in generateNumbers()
+def createFile(numbers, folder, file):
     with open(os.path.join(folder, file), "w") as newFile:
         newFile.write(numbers)
-    newFile.close()
 
-# C:\Users\Ryan\Desktop\SystemLanguages\Week5\Test
-# Test.txt
+# Asks for user inputs to pass as parameters along with generated numbers then creates the folder and file.
 def main():
     folder = input("Enter the folder name: ")
     createFolder(folder)
@@ -31,3 +33,6 @@ def main():
     createFile(numbers, folder, file)
 
 main()
+
+# C:\Users\Ryan\OneDrive\Desktop\SystemsLanguages\Week5\test
+# test.txt
