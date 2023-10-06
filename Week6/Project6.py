13d1f4ce28c044167b7b100f3b4db291c8a5d906
unknown <ryanrich811@gmail.com>
2023-10-06 10:03:22 -0400

    Completed workflow for program, testing still needed

--- Week6/Project6.py
+++ Week6/Project6.py
@@ -3,19 +3,26 @@ import platform, os
 def findOS():
     return platform.system()
 
-def isValidPath(OS, path):
+def checkPath(OS, path):
     if OS == "Windows":
         if '/' in path:
-            path.replace("/", "\")
-        else:
-            continue
+            path.replace("/", "\\")
+        return path
     elif OS == "Darwin":
-        if "\" in path:
-            path.replace("\", "/")
-        else:
-            continue
-    
-
+        if "\\" in path:
+            path.replace("\\", "/")
+        return path
+    return path
 
 def main():
     OS = findOS()
+    path = str(input("Enter a the path to your file: "))
+    checkPath(OS, path)
+
+    if os.path.exists(path):
+        print(os.path.basename(path))
+    else:
+        print("The path is not recognized.")
+
+main()
+
