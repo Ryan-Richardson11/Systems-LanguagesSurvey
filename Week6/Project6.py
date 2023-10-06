24fc287871b80f38ea968de42935f04bedd20c10
unknown <ryanrich811@gmail.com>
2023-10-06 09:54:03 -0400

    Starting logic for program

--- Week6/Project6.py
+++ Week6/Project6.py
@@ -1,1 +1,21 @@
 import platform, os
+
+def findOS():
+    return platform.system()
+
+def isValidPath(OS, path):
+    if OS == "Windows":
+        if '/' in path:
+            path.replace("/", "\")
+        else:
+            continue
+    elif OS == "Darwin":
+        if "\" in path:
+            path.replace("\", "/")
+        else:
+            continue
+    
+
+
+def main():
+    OS = findOS()
