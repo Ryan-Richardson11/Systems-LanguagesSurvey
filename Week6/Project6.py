ada002760ab7f2abd1ae56a6667b9a89be2c18fa
unknown <ryanrich811@gmail.com>
2023-10-07 22:39:20 -0400

    Added documentation for each function

--- Week6/Project6.py
+++ Week6/Project6.py
@@ -1,8 +1,11 @@
 import platform, os
 
+# Function returns the current systems platform
 def findOS():
     return platform.system()
 
+#Checks the path using the findOS function and an inputed path.
+# Switches the format to match the OS running. 
 def checkPath(OS, path):
     if OS == "Windows":
         if '/' in path:
@@ -14,11 +17,13 @@ def checkPath(OS, path):
         return path
     return path
 
+
 def main():
     OS = findOS()
     mypath = input("Enter a the path to your file: ")
     path = checkPath(OS, mypath)
 
+    # If the inputed path exists, prints out the base file name.
     if os.path.exists(path):
         print(os.path.basename(path))
     else:
@@ -26,4 +31,9 @@ def main():
 
 main()
 
+# Test Windows:
 # C:\\Users\\Ryan\\OneDrive\\Desktop\\AdvancedAlgorithms\\Week1\\Project1.py
+
+# Test MacOS:
+# C:/Users/Ryan/OneDrive/Desktop/AdvancedAlgorithms/Week1/Project1.py
+
